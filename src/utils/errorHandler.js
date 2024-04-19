531648a3824e03aca8abfcfe3a1d6a2da4f9e6bd
Kunal <151599404+Kunal782002@users.noreply.github.com>
2024-04-19 23:06:51 +0530

    Feat: Configured logging for file, console and mongo

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const BaseError = require("../errors/base.error");
+const logger = require("../config/logger.config")
 
 
 
@@ -14,7 +15,7 @@ function errorHandler(err, req, res, next) {
         });
     }
 
-
+    logger.error('Somethong went wrong')
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         success: false,
         message: 'Something went wrong',
